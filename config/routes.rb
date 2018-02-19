23cf19040beca1c96580094d8388c4d1f7fe52ff
Grant <grant.davis.reed@gmail.com>
2018-02-18 22:10:58 -0800

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 PostitTemplate::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
   get "comments/create"
   root to: 'posts#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
